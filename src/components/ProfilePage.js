c1d390e55b80f30e41220fdf983bffb2431a303b
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-18 18:10:13 +0100

    The home page for the App.

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function ProfilePage() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default ProfilePage;
