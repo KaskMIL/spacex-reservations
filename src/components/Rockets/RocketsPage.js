f9086848ad81f50cdf4c334f155575cf4e72c84d
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-18 18:12:50 +0100

    RocketPage for the Rockets

---
+++ src/components/Rockets/RocketsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+function RocketsPage() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default RocketsPage;
