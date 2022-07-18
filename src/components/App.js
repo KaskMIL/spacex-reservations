180e1abc8784b85b913986ce52be4d6eddf89a67
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-18 18:08:40 +0100

    App component for the App

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import DragonPage from './Dragon/DragonPage';
+import MissionPage from './Mission/MissionPage';
+import RocketsPage from './Rockets/RocketsPage';
+import ProfilePage from './ProfilePage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={(<ProfilePage />)}
+        />
+
+        <Route
+          path="/dragons"
+          element={<DragonPage />}
+        />
+
+        <Route
+          path="/missions"
+          element={<MissionPage />}
+        />
+
+        <Route
+          path="/rockets"
+          element={<RocketsPage />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
