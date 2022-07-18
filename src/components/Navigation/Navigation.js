d53180d6516781e786796e0616e2f093ea45335a
toMilanesi <tomi.milanesi@gmail.com>
2022-07-18 14:54:04 -0300

    Added header tag [to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,48 +6,50 @@ import styles from './Navigation.module.css';
 
 function Navigation() {
   return (
-    <nav className={styles.navContainer}>
+    <header className={styles.container}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
         <h1>Space Travelers&apos; hub</h1>
       </div>
-      <ul className={styles.list}>
-        <li>
-          <NavLink
-            to="/"
-            className={(Link) => (Link.isActive ? 'active' : 'none')}
-          >
-            My Profile
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/dragons"
-            className={(Link) => (Link.isActive ? 'active' : 'none')}
-          >
-            Dragons
-          </NavLink>
-        </li>
+      <nav>
+        <ul className={styles.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={(Link) => (Link.isActive ? 'active' : 'none')}
+            >
+              My Profile
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dragons"
+              className={(Link) => (Link.isActive ? 'active' : 'none')}
+            >
+              Dragons
+            </NavLink>
+          </li>
 
-        <li>
-          <NavLink
-            to="/missions"
-            className={(Link) => (Link.isActive ? 'active' : 'none')}
-          >
-            Missions
-          </NavLink>
-        </li>
+          <li>
+            <NavLink
+              to="/missions"
+              className={(Link) => (Link.isActive ? 'active' : 'none')}
+            >
+              Missions
+            </NavLink>
+          </li>
 
-        <li>
-          <NavLink
-            to="/rockets"
-            className={(Link) => (Link.isActive ? 'active' : 'none')}
-          >
-            Rockets
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <NavLink
+              to="/rockets"
+              className={(Link) => (Link.isActive ? 'active' : 'none')}
+            >
+              Rockets
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
