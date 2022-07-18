2cfffbe7e4acecadf8b782dae9ffb5c37f753b6d
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-18 18:10:45 +0100

    DragonPage to render

---
+++ src/components/Dragon/DragonPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+function DragonPage() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default DragonPage;
