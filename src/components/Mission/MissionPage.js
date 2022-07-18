b19aef106164c6d36dc1c0a0a552d30628080e80
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-18 18:11:02 +0100

    MissionPage for the app.

---
+++ src/components/Mission/MissionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+function MissionPage() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default MissionPage;
