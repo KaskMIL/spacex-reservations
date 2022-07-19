2f1705228b0bfa76055a804d846d6b3453402ffa
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-19 09:30:55 +0100

    renamed the rocket file to the rocket API but it also includes the reducer

---
+++ src/Redux/RocketAPIReducer.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH = 'rocketstore/rocket/FETCH'
+const API = 'https://api.spacexdata.com/v3/rockets'
+export const getRocketAPI = createAsyncThunk(FETCH, async () => {
+  const endPoint = await fetch(API, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8', 
+    },
+  });
+  let result = await endPoint.json()
+  console.log(result);
+  return result
+});
+
+// Slice Reducer
+
+const initializeState = [];
+
+const sliceRocket = createSlice({
+  name: 'rockets',
+  initialState: initializeState,
+  extraReducers: {
+    [getRocketAPI.fulfilled]: (state, action) => {
+      const rockets = [];
+      action.payload.forEach((rocket) => {
+        const data = {
+          rocket_id: rocket.id,
+          rocket_name: rocket.rocket_name,
+          rocket_type: rocket.type,
+          rocket_description: rocket.description,
+          rocket_image: rocket.flickr_images[1],
+        };
+        rockets.push(data);
+      });
+      return rockets;
+    },
+  },
+});
+
+export default sliceRocket.reducer;
