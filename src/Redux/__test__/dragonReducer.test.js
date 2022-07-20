7077c0cc50f5dceccb7361a623fa6c8bc723ee7f
toMilanesi <tomi.milanesi@gmail.com>
2022-07-20 17:27:43 -0300

    Fix linters errors

--- src/Redux/__test__/dragonReducer.test.js
+++
@@ -1,9 +0,0 @@
-import reducer, { initialState } from '../DragonsReducerSlice';
-
-test('should return initial state', () => {
-  expect(
-    reducer(undefined, {
-      type: undefined,
-    }),
-  ).toEqual(initialState);
-});
