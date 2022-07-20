70cf9d5a3fcd3cd3bfa2da889dee6b1ad487bbd7
Brenda309 <brewihogora23@gmail.com>
2022-07-20 02:03:06 +0200

    display the reserved missions on the profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Navigation from './Navigation/Navigation';
 
 function ProfilePage() {
+  const missionProfile = useSelector((state) => state.missions);
+  const missions = missionProfile;
+  const missionsReserved = missions.filter((data) => data.reserved === true);
+  let missionCounter = 0;
+
+  missions.map((data) => {
+    if (data.reserved) {
+      missionCounter += 1;
+    }
+    return missionCounter;
+  });
   return (
     <div>
       <Navigation />
+      <div className="profile-sect">
+        <h1>My Missions</h1>
+        <ul className="missionsBooked">
+          {missionCounter > 0 ? (
+            missionsReserved.map((data) => (
+              <li className="missionLi" key={data.mission_id}>
+                {data.mission_name}
+              </li>
+            ))
+          ) : (
+            <li className="hide" style={{ border: 'none' }}>
+              reserved missions
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
