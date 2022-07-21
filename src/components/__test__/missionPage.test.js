8542e42fd9049c473cf61aa30c7b2e58b32cb927
Brenda309 <brewihogora23@gmail.com>
2022-07-21 21:22:56 +0200

    Add test for the elements to shown on screen

---
+++ src/components/__test__/missionPage.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import Missions from '../Mission/MissionPage';
+import store from '../../Redux/configState';
+
+describe('Tests for the Missions component', () => {
+  render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  );
+  test('expected to have the following text to appear on screen', () => {
+    expect(screen.getByText('Missions')).toBeInTheDocument();
+    expect(screen.getByText('Description')).toBeInTheDocument();
+    expect(screen.getByText('Status')).toBeInTheDocument();
+  });
+//   test(' the snapshot', () => {
+//     expect(screen.debug()).toMatchSnapshot();
+//   });
+});
