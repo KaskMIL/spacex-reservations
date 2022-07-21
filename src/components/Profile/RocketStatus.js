3dbb035f66e29ddf97b4c79f3f3c742c3eb0bd9d
toMilanesi <tomi.milanesi@gmail.com>
2022-07-21 12:37:19 -0300

    Fix show 0 reservation on profile

--- src/components/Profile/RocketStatus.js
+++ src/components/Profile/RocketStatus.js
@@ -3,11 +3,10 @@ import { useSelector } from 'react-redux';
 
 function RocketStatus() {
   const rockets = useSelector((state) => state.rockets);
-  const rocketsReserved = rockets.filter((rocket) => rocket.reserved === false);
   let counter = 0;
 
   rockets.map((data) => {
-    if (data.reserved) {
+    if (!data.reserved) {
       counter += 1;
     }
     return counter;
@@ -18,19 +17,22 @@ function RocketStatus() {
       <div className="profile-sect">
         <h1 className="header">My Rockets</h1>
         <ul className="missionsBooked">
-          {counter <= 0
-            ? (
-              <li className="hide" style={{ border: 'none' }}>
-                Reserved Rockets Currently(0)
-              </li>
-            )
-            : (
-              rocketsReserved.map((rocket) => (
-                <li className="missionLi" key={rocket.rocketId}>
-                  {rocket.rocketName}
-                </li>
-              ))
-            ) }
+          {counter > 0 ? (
+            rockets.map((rocket) => {
+              if (rocket.rocketId) {
+                return (
+                  <li className="missionLi" key={rocket.rocketId}>
+                    {rocket.rocketName}
+                  </li>
+                );
+              }
+              return null;
+            })
+          ) : (
+            <li className="hide" style={{ border: 'none' }}>
+              Reserved Rockets Currently(0)
+            </li>
+          )}
         </ul>
       </div>
     </>
