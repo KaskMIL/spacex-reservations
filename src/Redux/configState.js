fe8c2f6ac9d0aa8cd4084abad54b8c2009a7ea30
toMilanesi <tomi.milanesi@gmail.com>
2022-07-21 16:45:13 -0300

    Added redux-logger and confiState

--- src/Redux/configState.js
+++ src/Redux/configState.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { logger } from 'redux-logger';
 import dragonsReducer from './DragonReducer';
 import { rocketReducer } from './RocketAPIReducer';
 import reducerMissions from './mission';
@@ -11,6 +12,7 @@ const Store = configureStore({
     rockets: rocketReducer,
     dragons: dragonsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default Store;
